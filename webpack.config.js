5cb1b7704ad348cd813e82a78940e99465715ff2
samahri <saud.amahri@gmail.com>
2023-02-04 17:26:31 +0300

    set up react

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './index.js', // the application's main file
+    mode: 'development',
+    output: { // specifiy where to the output files after bundling
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index_bundle.js',
+    },
+    target: 'web', // specifies where the app will run
+    devServer: { // configure the development server settings
+        port: '5001',
+        static: { // from where static files are served
+            directory: path.join(__dirname, 'public')
+        },
+        open: true, // automatically open the browser when the script runs
+        hot: true, // enable hot swapping: don't need to relaunch when the app changes
+        liveReload: true, // reloads the browser when there's a change
+    },
+    resolve: { // tells Webpack which types of files to consider when building our app
+        extensions: ['.js', '.jsx', '.json'],
+    },
+    module: { // rules to handle types of files
+        rules: [
+            { // use babel-loader when confronting js or jsx files
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader', // we haven't installed babel
+            },
+        ],
+    },
+    plugins: [ // use external plugins
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'public', 'index.html')
+        })
+    ]
+};
